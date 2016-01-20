8bf4c287d566bbd259870d974f734813e5ec70f1
Puluto <puluto@gmail.com>
2016-01-21 00:43:45 +0800

    Update influxdbHandler.py
    修复influxHandler对influxdb 0.9版本的支持.

--- src/diamond/handler/influxdbHandler.py
+++ src/diamond/handler/influxdbHandler.py
@@ -170,9 +170,10 @@ class InfluxdbHandler(Handler):
                 metrics = []
                 for path in self.batch:
                     metrics.append({
-                        "points": self.batch[path],
-                        "name": path,
-                        "columns": ["time", "value"]})
+                            "measurement": path,
+                            "precision": self.time_precision,
+                            "time": self.batch[path][0][0],
+                            "fields": {"value": self.batch[path][0][1]}})
                 # Send data to influxdb
                 self.log.debug("InfluxdbHandler: writing %d series of data",
                                len(metrics))
